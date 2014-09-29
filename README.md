3916c25ea8db81f682f495271812308aa284b089
senghuot <senghuotlim@gmail.com>
2014-09-28 23:48:55 -0700

    Adding get Smallest number in an integer array.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is where I includes all solutions to technical interview questions. All questions are mostly coming from "Cracking the Coding Interview".
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	
+	// Starting out easy
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1};
+		System.out.println(getSmallest(array));
+	}
+	
+	// Return the smallest number in an integer array
+	public static int getSmallest(int[] array) {
+		if (array.length == 0)
+			throw new IllegalArgumentException("You passed an empty array.");
+		
+		int smallest = array[0];
+		for(int i = 1; i < array.length; i++) {
+			smallest = Math.min(smallest, array[i]);
+		}
+		return smallest;
+	}
+}
