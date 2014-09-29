8b9e682cfa2de849b0a90b4c2caac6a8a10504b6
senghuot <senghuotlim@gmail.com>
2014-09-29 00:11:15 -0700

    Removing bin and add couting Island problem

---
+++ src/Island.java
@@ -0,0 +1,12 @@
+
+public class Island {
+	private int[][] Island;
+	
+	public static int getBlackPixels(int[][] pictures) {
+		return -1;
+	}
+	
+	private static int getBlackPixels(int[][] pictures, int curX, int curY) {
+		return -1;
+	}
+}
