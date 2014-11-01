e53ae6b98a08ae178965abbd01b287835277ae51
senghuot <senghuotlim@gmail.com>
2014-10-31 20:43:24 -0700

    Bit shift

--- src/google/Main.java
+++ src/google/Main.java
@@ -2,4 +2,13 @@ package google;
 
 public class Main {
 
+	public static void main(String[] args) {
+		System.out.println(multiply4(4));
+	}
+	
+	// fast way to multiply by 4.
+	public static int multiply4(int num) {
+		return num << 2;
+	}
+	
 }
