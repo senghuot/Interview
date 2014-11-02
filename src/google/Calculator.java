5ebcf946dc3fd686c014b3458419dbd98558ffc8
senghuot <senghuotlim@gmail.com>
2014-11-01 21:35:05 -0700

    Adding calculator

---
+++ src/google/Calculator.java
@@ -0,0 +1,33 @@
+package google;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	private String expression;
+	private int index;
+	
+	public Calculator(String expression) {
+		this.expression = expression;
+		index = 0;
+	}
+	
+	public int read() {
+		if (index == expression.length())
+			return 1;
+		
+		switch (expression.charAt(index++)) {
+			case '*':
+				return 4;
+			case '+':
+				return 3;
+			default:
+				return 2;
+		}
+	}
+	
+	public int getInt() {
+		return expression.charAt(index - 1) - 48;
+	}
+	
+}
--- src/google/Main.java
+++ src/google/Main.java
@@ -1,13 +1,54 @@
 package google;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(multiply4(4));
+		Calculator cal = new Calculator("9*1+2*9");
+//		System.out.println(cal.read());
+//		System.out.println(cal.read());
+//		System.out.println(cal.read());
+//		System.out.println(cal.read());
+//		System.out.println(cal.read());
+//		System.out.println(cal.getInt());
+		
+		Stack<Integer> num = new Stack<Integer>();
+		Stack<Integer> operator = new Stack<Integer>();
+		
+		int value;
+		while((value = cal.read()) != 1){
+			switch (value){
+				case 3:
+					operator.push(value);
+               break;
+				case 4:
+					cal.read();
+					int intFromNum = num.pop();
+					int intFromCal = cal.getInt();
+					int finalValue = intFromNum * intFromCal;
+					num.push(finalValue);
+               break;
+				default:
+					num.push(cal.getInt());
+			}
+		}
+		
+		while(!operator.isEmpty()){
+			operator.pop();
+			int v1 = num.pop();
+			int v2 = num.pop();
+			num.push(v1+v2);
+		}
+		
+		System.out.println(num);
 	}
 	
 	// fast way to multiply by 4.
 	public static int multiply4(int num) {
 		return num << 2;
 	}
+
 }
