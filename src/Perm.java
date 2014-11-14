9c10ec87a19fd7e8da33c61dd6b30e831f1b7203
Scottie Lim <senghuotlim@gmail.com>
2014-11-14 12:23:34 -0800

    adfasdf

--- src/Perm.java
+++ src/Perm.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Perm {
 
    public static void main(String[] args) {
-      System.out.println(permutation("abc"));
+      List<Integer> l = new ArrayList<Integer>();
+      l.add(1);
+      l.add(2);
+      l.add(3);
+      System.out.println(subSet(l));
    }
    
    public static List<String> permutation(String w) {
@@ -25,5 +29,30 @@ public class Perm {
          }
       }
       return res;
+   }
+   
+   public static List<List<Integer>> subSet(List<Integer> s) {
+   
+      List<List<Integer>> res = new ArrayList<List<Integer>>();
+      // empty base case
+      if (s.isEmpty()) {
+         List<Integer> tmp = new ArrayList<Integer>();
+         res.add(tmp);
+         return res;
+      }
+      
+      int first = s.get(0);
+      s.remove(0);
+      List<List<Integer>> tmp = subSet(s);
+      
+      for (List<Integer> l: tmp) {
+         List<Integer> a = new ArrayList<Integer>(l);
+         res.add(a);
+         l.add(first);
+         res.add(l);  
+      }
+   
+      return res;
+      
    }
 }
