7da799f443dd0f517ec357329fbd27bf9e5e889f
senghuot <senghuotlim@gmail.com>
2014-12-15 23:22:55 -0800

    adding some codesss

--- src/microsoft/Micrsoft.java
+++ src/microsoft/Micrsoft.java
@@ -1,5 +1,30 @@
 package microsoft;
 
 public class Micrsoft {
-	// will add all the solutions soon...im too lazy tonight
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public Pair Resize(int[] arr) throws Exception {
+		if (arr == null)
+			throw new IllegalArgumentException("Input can't be null.");
+		
+		Pair res = new Pair();
+		res.size = arr.length;
+		if (arr.length == 0)
+			return res;
+		
+		int maxTmp = arr[0];
+		for (int i = 1; i < arr.length; i++)
+			maxTmp = Math.max(maxTmp, arr[i]);
+		
+		res.max = maxTmp;
+		return res;
+	}
+	
+	class Pair {
+		public int size;
+		public int max;
+	}
 }
---
+++ src/microsoft/UpTree.java
@@ -0,0 +1,11 @@
+package microsoft;
+
+public class UpTree {
+
+	
+	public class Node {
+		private Node parent;
+		private Node left;
+		private Node right;
+	}
+}
